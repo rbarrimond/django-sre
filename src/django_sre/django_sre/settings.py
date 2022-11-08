4486b07303cdf23d3a5dddf843e41041d45a89ef
Robert Barrimond <robert@barrimond.com>
2022-11-07 23:17:19 -0500

    prometheus sqlite

--- src/django_sre/django_sre/settings.py
+++ src/django_sre/django_sre/settings.py
@@ -87,7 +87,8 @@ WSGI_APPLICATION = 'django_sre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django_prometheus.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
